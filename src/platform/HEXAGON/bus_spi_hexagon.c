11d150b34522e780e0c856b226e404402ff811bf
Eric Katzfey <eric.katzfey@modalai.com>
2025-04-24 13:24:07 -0700

    Cleaned up SPI error checking

--- src/platform/HEXAGON/bus_spi_hexagon.c
+++ src/platform/HEXAGON/bus_spi_hexagon.c
@@ -53,18 +53,49 @@ void spiInitDevice(SPIDevice device)
 // DMA transfer setup and start
 void spiSequenceStart(const extDevice_t *dev)
 {
-	printf("In spiSequenceStart");
+	if (dev == NULL) {
+		printf("**** NULL dev pointer");
+		return;
+	}
+
 	busDevice_t *bus = dev->bus;
-	int spi_fd = bus->busType_u.spi.instance->fd;
+
+	if (bus == NULL) {
+		printf("**** NULL bus pointer");
+		return;
+	}
+
+	SPI_TypeDef *instance = bus->busType_u.spi.instance;
+
+	if (instance == NULL) {
+		printf("**** NULL instance pointer");
+		return;
+	}
+
 	volatile struct busSegment_s *volatile curSegment = bus->curSegment;
 
+	if (curSegment == NULL) {
+		printf("**** NULL curSegment pointer");
+		return;
+	}
+
+	if (curSegment[0].negateCS == true) {
+		printf("**** Only a single record");
+		return;
+	}
+
 	if (curSegment[0].len != 1) {
-		printf("Register address is not length 1");
+		printf("**** Register address is not length 1");
+		return;
+	}
+
+	if (curSegment[1].negateCS != true) {
+		printf("**** more than 2 records");
 		return;
 	}
 
 	if (curSegment[1].len != 1) {
-		printf("Data is not length 1");
+		printf("**** Data is not length 1");
 		return;
 	}
 
@@ -77,32 +108,38 @@ void spiSequenceStart(const extDevice_t *dev)
 
 	// Send[0] has register address
 	send[0] = *curSegment[0].u.buffers.txData;
+
+	// A write transaction will have a non-NULL txData buffer
+	// and a read transaction will have a non-NULL rxData buffer
 	if (curSegment[1].u.buffers.txData) {
 		recv[1] = *curSegment[1].u.buffers.txData;
-		printf("Writing 0x%0.2x to register 0x%0.2x", recv[1], send[0]);
+		// printf("Writing 0x%0.2x to register 0x%0.2x", recv[1], send[0]);
 	} else if (curSegment[1].u.buffers.rxData) {
 		is_read = true;
 	}
+
+	int spi_fd = bus->busType_u.spi.instance->fd;
 
 	sl_client_spi_transfer(spi_fd, &send[0], &recv[0], len);
 
 	if (is_read) {
 		curSegment[1].u.buffers.rxData[0] = recv[1];
-		printf("Read 0x%0.2x from register 0x%0.2x", recv[1], (send[0] & 0x7F));
+		// printf("Read 0x%0.2x from register 0x%0.2x", recv[1], (send[0] & 0x7F));
 	}
 
+	// This marks the transaction as completed
 	dev->bus->curSegment = (busSegment_t *)BUS_SPI_FREE;
 }
 
 void spiPinConfigure(const spiPinConfig_t *pConfig)
 {
-	printf("In spiPinConfigure");
+	// printf("In spiPinConfigure");
 	(void) pConfig;
 }
 
 uint16_t spiCalculateDivider(uint32_t freq)
 {
-	printf("In spiCalculateDivider");
+	// printf("In spiCalculateDivider");
 	(void) freq;
 	return 1;
 }
