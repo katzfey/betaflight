752b06c8427a8cf8da5cd3c4835abba4be462580
Eric Katzfey <eric.katzfey@modalai.com>
2025-05-22 13:29:56 -0700

    Added OSD test script

--- modalai/deploy.sh
+++ modalai/deploy.sh
@@ -3,3 +3,5 @@
 adb push test.so /usr/lib/rfsa/adsp/betaflight.so
 adb push src/platform/HEXAGON/host/betaflight /usr/bin
 adb shell chmod a+x /usr/bin/betaflight
+adb push src/platform/HEXAGON/host/voxl-inspect-osd /usr/bin
+adb shell chmod a+x /usr/bin/voxl-inspect-osd
