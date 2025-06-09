02d8dbd26d6eed329d7626f01ec3122a107f5ff1
Eric Katzfey <eric.katzfey@modalai.com>
2025-06-09 14:58:34 -0700

    Reverted earlier unneeded format change

--- modalai/deploy.sh
+++ modalai/deploy.sh
@@ -5,4 +5,3 @@ adb push src/platform/HEXAGON/host/betaflight /usr/bin
 adb shell chmod a+x /usr/bin/betaflight
 adb push src/platform/HEXAGON/host/voxl-inspect-osd /usr/bin
 adb shell chmod a+x /usr/bin/voxl-inspect-osd
-
